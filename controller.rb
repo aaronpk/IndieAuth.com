14fd428c472a583f43fb35945162af8f31606213
Aaron Parecki <aaron@parecki.com>
2013-08-01 23:24:38 -0700

    enable the /verify route

--- controller.rb
+++ controller.rb
@@ -572,26 +572,26 @@ class Controller < Sinatra::Base
     json_response 200, {:me => login.user['href']}
   end
 
-  # get '/verify' do
-  #   if params[:token].nil?
-  #     json_error 400, {error: "invalid_request", error_description: "Missing 'token' parameter"}
-  #   end
+  get '/verify' do
+    if params[:token].nil?
+      json_error 400, {error: "invalid_request", error_description: "Missing 'token' parameter"}
+    end
 
-  #   login = Login.first :token => params[:token]
-  #   if login.nil?
-  #     json_error 404, {error: "invalid_token", error_description: "The token provided was not found"}
-  #   end
+    login = Login.first :token => params[:token]
+    if login.nil?
+      json_error 404, {error: "invalid_token", error_description: "The token provided was not found"}
+    end
 
-  #   if login.used_count > 0
-  #     json_error 400, {error: "expired_token", error_description: "The token provided has already been used"}
-  #   end
+    if login.used_count > 0
+      json_error 400, {error: "expired_token", error_description: "The token provided has already been used"}
+    end
 
-  #   login.last_used_at = Time.now
-  #   login.used_count = login.used_count + 1
-  #   login.save
+    login.last_used_at = Time.now
+    login.used_count = login.used_count + 1
+    login.save
 
-  #   json_response 200, {:me => login.user['href']}
-  # end
+    json_response 200, {:me => login.user['href']}
+  end
 
   get '/reset' do
     session.clear
