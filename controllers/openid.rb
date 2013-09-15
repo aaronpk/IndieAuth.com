8f5480488831f061e43ad523c3710e95e5a37aab
Aaron Parecki <aaron@parecki.com>
2013-09-15 11:52:55 -0700

    Stub openid flow, handling the first request, storing the openid parameters in the session and getting as far as a completed indieauth request.

--- controllers/openid.rb
+++ controllers/openid.rb
@@ -2,7 +2,20 @@ class Controller < Sinatra::Base
 
   get '/openid/?' do
     title "IndieAuth - OpenID Provider"
-    erb :openid
+
+    puts params.inspect
+
+    # Save all the openid.* parameters in a session, and redirect to /auth
+    session[:openid_params] = params.to_json
+
+    redirect "/auth?me=#{params['openid.identity']}&redirect_uri=#{URI.encode_www_form_component('/openid/complete')}"
+  end
+
+  get '/openid/complete' do
+    jj params
+    jj JSON.parse(session[:openid_params])
+
+    "Hello"
   end
 
 end
