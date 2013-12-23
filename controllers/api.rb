8afcadd852307c5f0dbe42bbc20975862bb350f6
Aaron Parecki <aaron@parecki.com>
2013-12-22 19:22:44 -0800

    implements endpoint for resources servers to verify auth codes

--- controllers/api.rb
+++ controllers/api.rb
@@ -56,4 +56,38 @@ class Controller < Sinatra::Base
     json_response 200, {:me => login.user['href']}
   end
 
+  # This is the POST route that handles verifying auth codes. It needs to match the name of the authorization URL
+  # otherwise we would need a link-rel tag to specify the location of this endpoint somewhere.
+  # This is for the "out of scope of OAuth 2.0" bit where the resource server verifies the code with the authorization server.
+  post '/auth' do
+    code = params[:code]
+
+    if code.nil?
+      json_error 400, {error: "invalid_request", error_description: "Missing 'code' parameter"}
+    end
+
+    login = Login.first :token => code
+    if login.nil?
+      json_error 404, {error: "invalid_request", error_description: "The code provided was not found"}
+    end
+
+    if login.used_count > 0
+      json_error 400, {error: "invalid_request", error_description: "The code provided has already been used"}
+    end
+
+    if login.redirect_uri != params[:redirect_uri]
+      json_error 400, {error: "invalid_request", error_description: "The 'redirect_uri' parameter did not match"}
+    end
+
+    if login.state != params[:state]
+      json_error 400, {error: "invalid_request", error_description: "The 'state' parameter did not match"}
+    end
+
+    login.last_used_at = Time.now
+    login.used_count = login.used_count + 1
+    login.save
+
+    json_response 200, {:me => login.user['href']}
+  end
+
 end
